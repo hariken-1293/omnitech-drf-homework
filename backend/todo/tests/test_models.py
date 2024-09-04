9a05e9bb19552da8b808280b5c7a5e5b28348300
hrkn63-hnm <demo43160@gmail.com>
2024-09-04 20:46:02 +0900

    add #4 ユニットテスト

---
+++ backend/todo/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+# Create your tests here.
+# backend/todo/tests/test_models.py
+import pytest
+from django.utils import timezone
+from todo.models import TodoItem
+
+@pytest.mark.django_db
+def test_todo_item_creation():
+    todo = TodoItem.objects.create(title="Test Todo", description="Test Description", completed=False)
+    assert todo.title == "Test Todo"
+    assert todo.description == "Test Description"
+    assert todo.completed is False
+
+@pytest.mark.django_db
+def test_todo_item_str_method():
+    todo = TodoItem.objects.create(title="Test Todo")
+    assert str(todo) == "Test Todo"
+
+@pytest.mark.django_db
+def test_todo_item_default_values():
+    todo = TodoItem.objects.create(title="Test Todo")
+    assert todo.description is None
+    assert todo.completed is False
