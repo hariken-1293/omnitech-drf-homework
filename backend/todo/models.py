725e26c265a077098fb22a7b47124da960c11a93
hrkn63-hnm <demo43160@gmail.com>
2024-09-07 17:24:10 +0900

    add #8 誰のTodoかわかるような実装

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+def get_default_user():
+    return User.objects.get(username='hariken') 
 
 # Create your models here.
 class TodoItem(models.Model):
     title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo_items')
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} (作成者: {self.user.username})"
